b2db8812397b28a844a0423408140712575d1b82
Kasper Kristensen (KSKR - Lektor - SK - SESG) <kskr@sesg.dk>
2024-02-06 08:59:44 +0100

    implemented score and turn logic

--- demo/src/main/java/com/example/classes/Logic.java
+++ demo/src/main/java/com/example/classes/Logic.java
@@ -1,31 +1,34 @@
 package com.example.classes;
 
 public class Logic {
-   int turn = 0; 
+   int turn = 0;
+   int myScore = 0; 
+   int enemyScore = 0;
 
-   final int saks = 0;
-   final int papir = 1;
-   final int sten = 2;
+   public static final int saks = 0;
+   public static final int papir = 1;
+   public static final int sten = 2;
 
-   final int draw = 3;
-   final int win = 4;
-   final int lose = 5;
+   public static final int draw = 3;
+   public static final int win = 4;
+   public static final int lose = 5;
 
    final int error = 9001;
 
    public int isWinner(int myChoose, int enemyChoose){
+    turn++;
     if (myChoose == saks){
         if (enemyChoose == saks)    {return draw;}
-        if (enemyChoose == papir)   {return win;}
-        if (enemyChoose == sten)    {return lose;}
+        if (enemyChoose == papir)   {myScore ++; return win;}
+        if (enemyChoose == sten)    {enemyScore++; return lose;}
     } else if (myChoose == papir){
-        if (enemyChoose == saks)    {return lose;}
+        if (enemyChoose == saks)    {enemyScore++;return lose;}
         if (enemyChoose == papir)   {return draw;}
-        if (enemyChoose == sten)    {return win;}
+        if (enemyChoose == sten)    {myScore++; return win;}
 
     } else if (myChoose == sten){
-        if (enemyChoose == saks)    {return win;}
-        if (enemyChoose == papir)   {return lose;}
+        if (enemyChoose == saks)    {myScore++; return win;}
+        if (enemyChoose == papir)   {enemyScore++; return lose;}
         if (enemyChoose == sten)    {return draw;}
     }
     
